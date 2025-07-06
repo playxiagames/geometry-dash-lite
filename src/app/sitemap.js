830c017a3023abc85e904db86d31918ed262ed49
pzh <190710500+playxiagames@users.noreply.github.com>
2025-07-07 01:05:38 +0800

    sitemap.xml 中增加 all-games 页面

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -16,6 +16,12 @@ export default function sitemap() {
       priority: 1.0,
     },
     {
+      url: `${baseUrl}/all-games/`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/about/`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
